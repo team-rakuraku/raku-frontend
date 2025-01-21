c97f10591c89cf5be0a1130a6c23122d2c483f7d
suojae <suojae.developer@gmail.com>
2025-01-21 18:10:10 +0900

    fix: auth-state 에서 freezed 제거 (#1)

--- lib/authentication/presentation/auth_state.dart
+++ lib/authentication/presentation/auth_state.dart
@@ -1,12 +1,25 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
 import '../domain/user_entity.dart';
 
-part 'auth_state.freezed.dart';
+abstract interface class AuthState {
+  const AuthState();
+}
 
-@freezed
-class AuthState with _$AuthState {
-  const factory AuthState.initial() = _Initial;
-  const factory AuthState.loading() = _Loading;
-  const factory AuthState.loggedIn(User user) = _LoggedIn;
-  const factory AuthState.error(String message) = _Error;
+class InitialAuthState extends AuthState {
+  const InitialAuthState();
+}
+
+class LoadingAuthState extends AuthState {
+  const LoadingAuthState();
+}
+
+class LoggedInAuthState extends AuthState {
+  final User user;
+
+  const LoggedInAuthState(this.user);
+}
+
+class ErrorAuthState extends AuthState {
+  final String message;
+
+  const ErrorAuthState(this.message);
 }
