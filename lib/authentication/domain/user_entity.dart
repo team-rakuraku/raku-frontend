688f7cdd395344b7568b70a90146f0114ee6bd14
suojae <suojae.developer@gmail.com>
2025-01-21 17:27:23 +0900

    feat: user-entity 추가 (#2)

---
+++ lib/authentication/domain/user_entity.dart
@@ -0,0 +1,11 @@
+final class User {
+  final String userId;
+  final String nickname;
+  final String profileImageUrl;
+
+  User({
+    required this.userId,
+    required this.nickname,
+    required this.profileImageUrl,
+  });
+}
