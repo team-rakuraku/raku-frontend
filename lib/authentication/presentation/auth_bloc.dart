fee462d89bafe019e6b1747c48c738fbb4f214dc
suojae <suojae.developer@gmail.com>
2025-01-22 20:18:04 +0900

    feat: auth-bloc 도메인 레이어 연결 및 freezed 반영 (#1)

--- lib/authentication/presentation/auth_bloc.dart
+++ lib/authentication/presentation/auth_bloc.dart
@@ -1,29 +1,35 @@
 import 'package:bloc/bloc.dart';
+import '../domain/usecases/auth_uscease.dart';
 import 'auth_event.dart';
 import 'auth_state.dart';
-import '../domain/user_entity.dart';
+import '../domain/entity/user_entity.dart';
 
-class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(const InitialAuthState()) {
-    on<AuthEvent>(_onLogin);
+final class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final AuthUseCase _authUseCase;
+
+  AuthBloc({required AuthUseCase authUseCase})
+      : _authUseCase = authUseCase,
+        super(const AuthState.initial()) {
+    on<LoginAuthEvent>(_onLogin);
   }
 
   /// 로그인 이벤트 처리
-  Future<void> _onLogin(AuthEvent event, Emitter<AuthState> emit) async {
-    emit(const LoadingAuthState());
-    try {
-      // TODO: 실제 로그인 로직 추가
-      final user = User(
-        userId: event.userParams.userId,
-        nickname: event.userParams.nickname,
-        profileImageUrl: event.userParams.profileImageUrl,
-      );
+  Future<void> _onLogin(LoginAuthEvent event, Emitter<AuthState> emit) async {
+    emit(const AuthState.loading());
 
-      // 로그인 성공 시 상태 업데이트
-      emit(LoggedInAuthState(user));
-    } catch (error) {
-      // 로그인 실패 시 에러 상태로 전환
-      emit(ErrorAuthState(error.toString()));
-    }
+    final user = User(
+      userId: event.userParams.userId,
+      nickname: event.userParams.nickname,
+      profileImageUrl: event.userParams.profileImageUrl,
+      accessToken: event.accessToken,
+      appId: event.appId,
+    );
+
+    final result = await _authUseCase.performLogin(user: user).run();
+
+    result.match(
+      (failure) => emit(AuthState.error(failure.userFriendlyMessage)),
+      (loggedInUser) => emit(AuthState.loggedIn(loggedInUser)),
+    );
   }
 }
