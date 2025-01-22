aa8e9aaed5b3f8a0b4d14e073d1ae9b33018c871
suojae <suojae.developer@gmail.com>
2025-01-22 20:28:27 +0900

    feature: SDK 초기 셋팅(authentication) 기능 구현 (#9)
    * feat: auth-service 추가 (#1)
    
    * feat: user-entity 추가 (#2)
    
    * feat: auth-state 추가 (#1)
    
    * package: 필수 패키지 추가
    
    * fix: auth-state 에서 freezed 제거 (#1)
    
    * fix: user-params 분리 및 public 전환 (#1)
    
    * feat: auth-event 추가 (#1)
    
    * feat: auth-bloc 추가 (#1)
    
    * feat: 모나드 에러처리 위한 failure 객체 추가 (#8)
    
    * package: json_serializable 패키지 추가 (#8)
    
    * chore: 미사용 파일 삭제
    
    * feat: login_dto 추가 (#8)
    
    * feat: api-endpoint 추가 (#8)
    
    * chore: user-entity 파일 위치이동 및 누락 속성 추가 (#2)
    
    * feat: user-usecase 추가 (#2)
    
    * feat: response-parser 추가 (#8)
    
    * feat: request-builder 추가 (#8)
    
    * feat: api-service 추가(#8)
    
    * feat: login_gateway 추가 (#8)
    
    * refactor: auth-event 에 freezed 도입 (#1)
    
    * refactor: auth-state 에 freezed 도입 (#1)
    
    * chore: 미사용 파일 삭제
    
    * feat: di-container 추가 (#8)
    
    * feat: auth-bloc 도메인 레이어 연결 및 freezed 반영 (#1)
    
    * feat: RakuChatSDK di container 이용해 의존성 주입 (#1)
    
    * feat: 진입점 생성
    
    * test: auth-usecase 단위테스트 추가 (#8)

---
+++ lib/authentication/di_container.dart
@@ -0,0 +1,46 @@
+import 'package:dio/dio.dart';
+import 'package:rakuraku/authentication/domain/data_interfaces/login_repository_interface.dart';
+import 'package:rakuraku/authentication/presentation/auth_bloc.dart';
+
+import '../services/api_service.dart';
+import 'data/gateway/api_endpoint.dart';
+import 'data/gateway/login_gateway.dart';
+import 'domain/usecases/auth_uscease.dart';
+
+class DIContainer {
+  DIContainer._();
+
+  static final DIContainer _instance = DIContainer._();
+
+  factory DIContainer() => _instance;
+
+  final Map<Type, dynamic> _services = {};
+
+  void register<T>(T Function() factory) {
+    _services[T] = factory;
+  }
+
+  T resolve<T>() {
+    final service = _services[T];
+    if (service is T Function()) {
+      final instance = service();
+      _services[T] = instance;
+      return instance;
+    } else if (service != null) {
+      return service;
+    }
+    throw Exception("Service of type $T is not registered in DIContainer.");
+  }
+}
+
+void initializeSDKDependencies() {
+  final di = DIContainer();
+
+  di.register<ApiService>(() => ApiService(
+        Dio(),
+        baseUrl: ApiEndpoint.baseUrl.path,
+      ));
+  di.register<LoginGatewayInterface>(() => LoginGateway(apiService: di.resolve<ApiService>()));
+  di.register<AuthUseCase>(() => AuthUseCase(loginGateway: di.resolve<LoginGateway>()));
+  di.register<AuthBloc>(() => AuthBloc(authUseCase: di.resolve<AuthUseCase>()));
+}
---
+++ lib/authentication/presentation/model/user_params.dart
@@ -0,0 +1,11 @@
+final class UserParams {
+  final String userId;
+  final String nickname;
+  final String profileImageUrl;
+
+  UserParams({
+    required this.userId,
+    required this.nickname,
+    required this.profileImageUrl,
+  });
+}
