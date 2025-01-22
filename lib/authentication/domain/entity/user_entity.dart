8568251aa737eb6a71aa88cc0f10dce32990f226
suojae <suojae.developer@gmail.com>
2025-01-22 20:14:30 +0900

    chore: user-entity 파일 위치이동 및 누락 속성 추가 (#2)

--- lib/authentication/domain/entity/user_entity.dart
+++ lib/authentication/domain/entity/user_entity.dart
@@ -1,10 +1,14 @@
 final class User {
   final String userId;
+  final String accessToken;
+  final String appId;
   final String nickname;
   final String profileImageUrl;
 
   User({
     required this.userId,
+    required this.accessToken,
+    required this.appId,
     required this.nickname,
     required this.profileImageUrl,
   });
