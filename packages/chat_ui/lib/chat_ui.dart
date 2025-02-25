bc51256b41d479f98a9897c9c92197fbe8345966
suojae <suojae.developer@gmail.com>
2025-02-25 21:44:50 +0900

    feat: chat-ui export 파일 추가 (#15)

--- packages/chat_ui/lib/chat_ui.dart
+++ packages/chat_ui/lib/chat_ui.dart
@@ -1,5 +1,8 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:chat_ui/chat_rooms/all_chat_rooms_view.dart';
+export 'package:chat_ui/chat_rooms/chat_room_state.dart';
+export 'package:chat_ui/chat_rooms/chat_rooms_event.dart';
+export 'package:chat_ui/chat/chat_bloc.dart';
+export 'package:chat_ui/chat/chat_event.dart';
+export 'package:chat_ui/chat/chat_state.dart';
+export 'package:chat_ui/chat/chat_view.dart';
+
