c0e4d92712d2c510cbe50fccc238028d446fe7d3
suojae <suojae.developer@gmail.com>
2025-02-25 20:26:42 +0900

    fix: ChatRoomCreateDto의 fromJson 중복 제거 및 type 필드 enum으로 변경 (#14)

--- packages/chat_sdk/lib/chat/data/remote/dto/chat_room_create_dto.dart
+++ packages/chat_sdk/lib/chat/data/remote/dto/chat_room_create_dto.dart
@@ -2,21 +2,34 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'chat_room_create_dto.g.dart';
 
+enum ChatRoomType {
+  @JsonValue("SINGLE")
+  single,
+
+  @JsonValue("GROUP")
+  group,
+}
+
 @JsonSerializable()
 final class ChatRoomCreateDto {
   final String userId;
   final String name;
-  final String type;
+
+  @JsonKey(unknownEnumValue: ChatRoomType.single)
+  final ChatRoomType type;
+
+  @JsonKey(defaultValue: [])
   final List<String> invitedUserIds;
 
   const ChatRoomCreateDto({
     required this.userId,
     required this.name,
     required this.type,
-    required this.invitedUserIds,
+    this.invitedUserIds = const [],
   });
 
-  factory ChatRoomCreateDto.fromJson(Map<String, dynamic> json) => _$ChatRoomCreateDtoFromJson(json);
+  factory ChatRoomCreateDto.fromJson(Map<String, dynamic> json) =>
+      _$ChatRoomCreateDtoFromJson(json);
 
   Map<String, dynamic> toJson() => _$ChatRoomCreateDtoToJson(this);
 }
